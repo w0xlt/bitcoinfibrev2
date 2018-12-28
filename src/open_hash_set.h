rustyrussell/bitcoin

open_hash_set: use a linear hash scan.
No more jumping around the table, and just use the lower bits as hash.

	Total insert hashes = 1762, total find hashes = 65245

Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

src/open_hash_set.h
@@ -39,10 +39,9 @@ private:
     std::vector<value_type> table;
     size_type count = 0, scan_max;
 
-    inline size_t hash_pos(uint64_t hash, size_t i) {
-        uint64_t input = hash * (i + 1);
-        uint32_t value = (input & 0xffffffffffLLU) ^ ((input & 0xffff00000000LLU) >> 16) ^ ((input & 0xffff000000000000LLU) >> 32);
-        return value & ((size_type(1) << bits)-1);
+    /* Hash should be uniform already. */
+    inline size_t hash_base(uint64_t hash) {
+        return hash & ((size_type(1) << bits)-1);
     }
 
     // Power of 2 which keeps us under 25 full.
@@ -61,15 +60,15 @@ public:
     {}
 
     std::pair<iterator, bool> insert(const value_type& value) {
-        size_t pos;
+        size_t pos = hash_base(hash_instance(value));
         size_t i = 0;
         while (i < scan_max) {
             total_insert_hashes++;
-            pos = hash_pos(hash_instance(value), i);
             if (null_instance(table[pos]))
                break;
             if (equal_instance(table[pos], value))
                break;
+            pos = (pos + 1)  & ((size_type(1) << bits)-1);
             i++;
         }
 
@@ -86,15 +85,15 @@ public:
     }
 
     iterator find(const value_type& value) {
-        size_t pos;
+        size_t pos = hash_base(hash_instance(value));
         size_t i = 0;
         while (i < scan_max) {
             total_find_hashes++;
-            pos = hash_pos(hash_instance(value), i);
             if (null_instance(table[pos]))
                break;
             if (equal_instance(table[pos], value))
                break;
+            pos = (pos + 1)  & ((size_type(1) << bits)-1);
             i++;
         }
 

w0xlt/bitcoinfibrev2

src/open_hash_set.h
@@ -40,10 +40,9 @@ private:
     std::vector<value_type> m_table;
     size_type m_count = 0, m_scan_max;
 
-    inline size_t hash_pos(uint64_t hash, size_t i) {
-        uint64_t input = hash * (i + 1);
-        uint32_t value = (input & 0xffffffffffLLU) ^ ((input & 0xffff00000000LLU) >> 16) ^ ((input & 0xffff000000000000LLU) >> 32);
-        return value & ((size_type(1) << m_bits)-1);
+    /* Hash should be uniform already. */
+    inline size_t hash_base(uint64_t hash) {
+        return hash & ((size_type(1) << m_bits)-1);
     }
 
     // Power of 2 which keeps us under 25 full.
@@ -63,12 +62,12 @@ public:
     {}
 
     std::pair<iterator, bool> insert(const value_type& value) {
-        size_t pos;
+        size_t pos = hash_base(m_hash_instance(value));
         size_t i = 0;
         while (i < m_scan_max) {
-            pos = hash_pos(m_hash_instance(value), i);
             if (m_null_instance(m_table[pos])) break;
             if (m_equal_instance(m_table[pos], value)) break;
+            pos = (pos + 1)  & ((size_type(1) << m_bits)-1);
             i++;
         }
 
@@ -86,12 +85,12 @@ public:
     }
 
     iterator find(const value_type& value) {
-        size_t pos;
+        size_t pos = hash_base(m_hash_instance(value));
         size_t i = 0;
         while (i < m_scan_max) {
-            pos = hash_pos(m_hash_instance(value), i);
             if (m_null_instance(m_table[pos])) break;
             if (m_equal_instance(m_table[pos], value)) break;
+            pos = (pos + 1)  & ((size_type(1) << m_bits)-1);
             i++;
         }
 